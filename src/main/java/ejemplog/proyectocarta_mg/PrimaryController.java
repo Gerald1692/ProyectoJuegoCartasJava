443708bfc6abb2d0f557757d5cdb6a8835489768
Gerald1692 <geraldariasgarcia06@gmail.com>
2025-06-18 15:53:54 -0600

    Se agrego botones funcionales en la parte visual
    Se agrego un botón con una validación de prueba en la parte de Primary y se estudio un poco la parte grafica

--- src/main/java/ejemplog/proyectocarta_mg/PrimaryController.java
+++ src/main/java/ejemplog/proyectocarta_mg/PrimaryController.java
@@ -1,7 +1,10 @@
 package ejemplog.proyectocarta_mg;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 public class PrimaryController {
 
@@ -9,4 +12,29 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    
+      @FXML
+    private void MostrarMensaje(ActionEvent event){
+        Alert comfimar =new Alert(Alert.AlertType.CONFIRMATION);
+        comfimar.setTitle("Bienvenido ");
+        comfimar.setHeaderText("Estas seguro?");
+        comfimar.setContentText("Aceptar para continuar ");
+        comfimar.showAndWait().ifPresent(respuesta->{
+        
+        if (respuesta==ButtonType.OK){
+            System.out.println("Seleccionaste Ok");
+            
+        }
+        else{
+            System.out.println("Operacion Cancelada");
+        }
+            
+        
+        
+        
+        });
+        
+    }
 }
+
+
