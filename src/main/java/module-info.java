30ac60179a0e89a02cb205d36d91d55e1b3086a2
Marcela Abarca Delgado <admarcela5@gmail.com>
2025-06-23 17:53:22 -0600

    de aqui vamos con el profe
    de aqui vamos con el profe

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module ejemplog.proyectocarta_mg {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens ejemplog.proyectocarta_mg to javafx.fxml;
-    exports ejemplog.proyectocarta_mg;
-}
