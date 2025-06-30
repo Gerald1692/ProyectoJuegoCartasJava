cd831e762038dd8f567f027398ebd89e18cd7168
Marcela Abarca Delgado <admarcela5@gmail.com>
2025-06-29 21:09:43 -0600

    cargar imagenes de caras
    se crea una clase metodo y collections para poner las imagenes en los botones ya solo falta pasarlo al controlador

--- src/main/java/ejemplog/proyectocarta_mg/Tablero.java
+++ src/main/java/ejemplog/proyectocarta_mg/Tablero.java
@@ -3,12 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ejemplog.proyectocarta_mg;
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
-import java.io.ObjectOutputStream;
-import java.io.ObjectInputStream;
+import javafx.scene.image.Image;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.io.Serializable;
-import java.io.IOException;
 
 /**
  *
@@ -22,24 +21,42 @@ import javafx.scene.image.Image;
 public class Tablero implements Serializable{
     
     private Carta [][] cartas;
-    
     private int filas=4;
     private int columnas=3;
     
+    
+    private List<Image> imagenesNormales;
+    private List<Image> imagenesBonus;
+    private List<Image> imagenesCastigo;
+    
+    
+    
     public Tablero(){
+        imagenesNormales = CargadorImagenes.cargarDesdeCarpeta("imagenes/ImgNormal");
+        imagenesBonus = CargadorImagenes.cargarDesdeCarpeta("imagenes/ImgBonus");
+        imagenesCastigo = CargadorImagenes.cargarDesdeCarpeta("imagenes/Castigo");
         inicializarCartas();
     }
+    
+    
     
     private void inicializarCartas(){
         cartas = new Carta[filas][columnas];
         int totalCartas = filas * columnas;
         List<Carta> listaCartas = new ArrayList<>();
         
+       
+        
+        int indiceNormal= 0;
+        int indiceBonus= 0;
+        int indiceCastigo= 0;
+        
+        
         for (int i=0; i<4;i++){
             
             String id = "N"+i;
             
-            Image imagenCarta=null;
+            Image imagenCarta=imagenesNormales.get(indiceNormal++);;
             
             Carta carta1 = new CartaNormal(id, Carta.EstadoCarta.Oculta, Carta.TipoCarta.Normal,imagenCarta);
             Carta carta2 = new CartaNormal( id, Carta.EstadoCarta.Oculta, Carta.TipoCarta.Normal, imagenCarta);
@@ -52,7 +69,7 @@ public class Tablero implements Serializable{
             
             String id = "B"+i;
             
-            Image imagenCarta=null;
+            Image imagenCarta= imagenesBonus.get(indiceBonus++);
             
             Carta carta1 = new CartaBonus(id, Carta.EstadoCarta.Oculta, Carta.TipoCarta.Bonus, imagenCarta);
             Carta carta2 = new CartaBonus(id, Carta.EstadoCarta.Oculta, Carta.TipoCarta.Bonus, imagenCarta);
@@ -65,12 +82,12 @@ public class Tablero implements Serializable{
         for (int i = 0;i <1; i++ ){
             String id = "C"+i;
             
-            Image imagenCarta=null;
+            Image imagenCarta=imagenesCastigo.get(indiceCastigo);
             
             Carta carta1 = new CartaCastigo(id, Carta.EstadoCarta.Revelada, Carta.TipoCarta.Castigo, imagenCarta);
             Carta carta2 = new CartaCastigo(id, Carta.EstadoCarta.Revelada, Carta.TipoCarta.Castigo, imagenCarta);
             listaCartas.add(carta1);
-            listaCartas.add(carta2.clonar().clonar());
+            listaCartas.add(carta2.clonar());
         }
         
         Collections.shuffle(listaCartas);
